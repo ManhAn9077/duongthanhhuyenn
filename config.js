b2e1b20907ae9f8f4f935d224cb74a5e640468af
ManhAn9077 <94302143+ManhAn9077@users.noreply.github.com>
2023-10-20 22:03:48 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Huyền Dương';
+const giftUrl = '#';
+const eventName = 'Chúc Mừng 20/10';
+const titleCard = 'To Dương Thanh Huyền';
+const contentCard = 'Chúc bạn có  ngày 20/10 luôn luôn vui vẻ tràn ngập tiếng cười trong hôm nay và cả những ngày mai sau nữa';
+
+
+const giftImage = 'https://scontent.fhan2-3.fna.fbcdn.net/v/t1.15752-9/372151733_983298222950960_6294597939677640473_n.png?_nc_cat=106&ccb=1-7&_nc_sid=8cd0a2&_nc_ohc=zyOMvC2asXIAX-fllv-&_nc_ht=scontent.fhan2-3.fna&oh=03_AdTWgT0MvyxDtzV9Z9BaEdGmF8I9nPukNG7K6k6rgg7kHw&oe=6559E74C';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
